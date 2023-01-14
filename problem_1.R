27f38bc25beea8b8b0318263834d4a7d9d67b70c
Aiden McCall <120305131+aiden-mccall@users.noreply.github.com>
2023-01-14 16:13:23 +0000

    Add files via upload

---
+++ problem_1.R
@@ -0,0 +1,22 @@
+problem_1 <- function (number){
+  # Starts a counter
+  sum.count <- 0
+  # Loops for the input (user defined)
+  for (i in 1:number){
+    # If i modulo 3 is 0 then divisible by 3.
+    if (i %% 3 == 0){
+      sum.count = sum.count + i
+    }
+    # If i modulo 5 is 0 then divisible by 5.
+    else if (i %% 5 == 0){
+      sum.count = sum.count + i
+    }
+    # Just returns the same sum.
+    else sum.count = sum.count
+  }
+  # Prints the sum answer to console.
+  sum.count
+}
+
+# Answer
+problem_1(999)
