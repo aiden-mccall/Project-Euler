d1eb3fa341785efc31c010f1bc3ed940a90266b3
Aiden McCall <120305131+aiden-mccall@users.noreply.github.com>
2023-03-21 09:22:54 +0000

    Update problem_5.f95

--- Fortran/problem_5.f95
+++ Fortran/problem_5.f95
@@ -7,6 +7,10 @@ integer :: num
 
 num = 1
 DO WHILE (num > 0)
+	! To note this is done so all are included, you don't need to do this.
+	! It follows mathematically that if a number has a prime factor 2 for example
+	! then MOD(num,2) is already statisfied. Using this you don't need to test all cases
+	! I included all for completeness and ease to the viewer.
 	IF ( MODULO(num,2) == 0 .and. MODULO(num,3) == 0 .and. MODULO(num,4) == 0 &
 	& .and. MODULO(num,5) == 0 .and. MODULO(num,6) == 0 .and. MODULO(num,7) == 0 &
 	& .and. MODULO(num,8) == 0 .and. MODULO(num,9) == 0 .and. MODULO(num,10) == 0 &
